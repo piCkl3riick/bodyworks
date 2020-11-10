be072e1b4926ce43c51216d94abd6691371d6b34
Luke-Reece Philander <lukereecephilander21@gmail.com>
2020-11-10 16:24:12 +0200

    Navigation Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { Router, NavigationEnd } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'universalink';
+export class AppComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  title = "universalink";
+
+  ngOnInit() {
+    this.router.events.subscribe((evt) =>{
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      try {
+        window.scrollTo({left: 0, top: 0, behavior: 'smooth'});
+
+      } catch(e) {
+        window.scroll(0, 0);
+      }
+    })
+  }
+
 }
+
+
+
+
